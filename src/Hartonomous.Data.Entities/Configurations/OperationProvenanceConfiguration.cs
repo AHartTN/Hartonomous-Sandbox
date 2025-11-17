e06a940ebc1d6cc213a67c2fcb83d04a8624419e
Anthony Hart <aharttn@gmail.com>
2025-11-17 17:04:44 -0600

    fix: Clean Interfaces/ folder during scaffolding (generated code causes build failures)

--- src/Hartonomous.Data.Entities/Configurations/OperationProvenanceConfiguration.cs
+++ src/Hartonomous.Data.Entities/Configurations/OperationProvenanceConfiguration.cs
@@ -32,7 +32,7 @@ public class OperationProvenanceConfiguration : IEntityTypeConfiguration<Operati
             ;
 
         builder.HasIndex(e => new { e.OperationId })
-            .HasDatabaseName("UQ__Operatio__A4F5FC4568462F78")
+            .HasDatabaseName("UQ__Operatio__A4F5FC45804953F5")
             .IsUnique()
             ;
     }
